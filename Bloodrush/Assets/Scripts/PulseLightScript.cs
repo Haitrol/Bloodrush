ae5cc6c75c6370a2e267a1ee8d43c88d7cf3a6fb
Benjamin Vehling <silverplay@hotmail.com>
2016-10-13 00:01:12 +0200

    Partikel test, Body Screen und Sprach test

---
+++ Bloodrush/Assets/Scripts/PulseLightScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulseLightScript : MonoBehaviour
+{
+    public float pulseDistance = 2;
+    public float pulseSpeed = 10;
+
+    Light light;
+
+    void Start()
+    {
+        light = gameObject.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        light.intensity = Mathf.Sin(Time.time * pulseSpeed) * pulseDistance;
+    }
+}
