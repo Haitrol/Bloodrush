28cb593195beec45fa6fef49e8416fbef07d7809
Unknown <schmid.sven@hotmail.com>
2016-10-13 13:11:30 +0200

    BloodCellFeedback

---
+++ Bloodrush/Assets/Scripts/BkFallandRemove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BkFallandRemove : MonoBehaviour {
+
+	public float fallSpeed = 8.0f;
+	public float spinSpeed = 250.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		transform.Translate (Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+		transform.Rotate (Vector3.forward, spinSpeed * Time.deltaTime);
+	
+	}
+	
+	// Update is called once per frame
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	
+	}
+}
---
+++ Bloodrush/Assets/Scripts/BloodRain.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodRain : MonoBehaviour 
+{
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for (int i = 0; i < 10; i++)
+			Instantiate (prefab, new Vector3 (i * 2.0f, 0, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
